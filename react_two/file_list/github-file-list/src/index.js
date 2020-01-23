3af6e7b7a0003c88b5fe878fd5012eb01754bb33
Teorell <fedorizmalkov@gmail.com>
2020-01-24 00:27:04 +0300

    more react

--- react_two/file_list/github-file-list/src/index.js
+++ react_two/file_list/github-file-list/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Time from './time';
 
 const testFiles = [
     {
@@ -34,21 +34,104 @@ message: 'Added a readme'
 },
 ];
 
+const FileListItem = ({ file }) => (
+    <tr className="file-list-item">
+        <td><FileIcon file={file} /></td>
+        <td><FileName file={file} /></td>
+        <td><CommitMessage commit={file.latestCommit}/></td>
+        <td><Time time={file.updated_at} /></td>
+    </tr>
+)
 
-const FileList = ({ files }) = (
+const FileIcon = ({ file }) => {
+    let icon = 'fa-file-text-o';
+
+    if (file.type === 'folder'){
+        icon = 'fa-folder';
+    }
+
+    return (
+    <div className="file-icon">
+        <i className={`fa ${icon}`}/>
+    </div>
+    );
+}
+
+const FileName = ({ file }) => (
+    <div className="file-name">
+        {file.name}
+    </div>
+)
+
+// function getFileName(file){
+//     return [
+//         <FileIcon file={file} key={0}/>,
+//         <td className="file-name" key={1}>{file.name}</td>
+//     ];
+// }
+
+const CommitMessage = ({ commit }) => (
+    <div className="commit-message">
+        {commit.message}
+    </div>
+);
+
+const FileList = ({ files }) => (
     <table className="file-list">
         <tbody>
             {files.map(file => (
-                <tr className="file-list-item" key={file.id}>
-                    <td className="file-name">{file.name}</td>
-                </tr>
+                <FileListItem key={file.id} file={file}/>
             ))}
         </tbody>
     </table>
+)
+
+const data = [
+    {
+        id: 0,
+        feature: 'Subwoofer',
+    },
+    {
+        id: 1,
+        feature: 'Non-porous , washable',
+    },
+    {
+        id: 2,
+        feature: 'Wings',
+    },
+    {
+        id: 3,
+        feature: 'Beveled Bezel',
+    },
+    {
+        id: 4,
+        feature: 'Bezeled Bevel',
+    },
+    {
+        id: 5,
+        feature: 'Seedless',
+    }
+];
+
+const FeatureItem = ({item}) => (
+    <tr>
+        <td className="featureItem">{item.feature}</td>
+    </tr>
+)
+
+const PhoneFeatures = ({ data }) => (
+        <table className="phoneFeatures">
+            <tr className="headerName">Phone Features</tr>
+            <tbody>
+                {data.map(item => (
+                    <FeatureItem item={item} key={item.id} />
+                ))}
+            </tbody>
+        </table>
 )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-ReactDOM.render(<FileList files={testFiles} />, document.getElementById('root'));
+ReactDOM.render(<PhoneFeatures data={data} />, document.getElementById('root'));
